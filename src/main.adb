ee11af420f92ca742b104e93e5f5cb2d59a96fae
Sebastian Baginski <s.baginski@mail.com>
2019-04-07 15:20:58 +0200

    test histograms

--- src/main.adb
+++ src/main.adb
@@ -133,16 +133,47 @@ begin
          declare
             h0: Histogram.Data := HistogramGenerator.verticalProjection(testImage, regions.Element(it).area);
             h1: Histogram.Data := HistogramGenerator.horizontalProjection(testImage, regions.Element(it).area);
-            h0_res: Histogram.Data(10);
-            h1_res: Histogram.Data(10);
+            h0_res: Histogram.Data(20);
+            h1_res: Histogram.Data(20);
+
+            h0_best: Float := 1000.0;
+            h1_best: Float := 1000.0;
+            h0_id: Integer := 0;
+            h1_id: Integer := 0;
          begin
-            h0_res := h0.resized(10);
-            h1_res := h1.resized(10);
+            h0_res := h0.resized(h0_res.size);
+            h1_res := h1.resized(h1_res.size);
             h0_res.normalize;
             h1_res.normalize;
-            h0_res.print;
-            h1_res.print;
             Ada.Text_IO.Put_Line("");
+            for j in 0 .. Integer(regions.Length - 1) loop
+               if j /= it then
+                  declare
+                     h0_j: Histogram.Data := HistogramGenerator.verticalProjection(testImage, regions.Element(j).area);
+                     h1_j: Histogram.Data := HistogramGenerator.horizontalProjection(testImage, regions.Element(j).area);
+                     h0_j_res: Histogram.Data(h0_res.size);
+                     h1_j_res: Histogram.Data(h1_res.size);
+                     method: Histogram.CompareMethod := Histogram.Bhattacharyya;
+                     dd1, dd2: Float;
+                  begin
+                     h0_j_res := h0_j.resized(h0_res.size);
+                     h1_j_res := h1_j.resized(h1_res.size);
+                     h0_j_res.normalize;
+                     h1_j_res.normalize;
+                     dd1 := h0_res.compare(h0_j_res, method);
+                     dd2 := h1_res.compare(h1_j_res, method);
+                     if dd1 < h0_best then
+                        h0_best := dd1;
+                        h0_id := j;
+                     end if;
+                     if dd2 < h1_best then
+                        h1_best := dd2;
+                        h1_id := j;
+                     end if;
+                  end;
+               end if;
+            end loop;
+            Ada.Text_IO.Put_Line(it'Image & " matches best: " & h0_id'Image & ", " & h1_id'Image & " -> " & h0_best'Image & ", " & h1_best'Image);
          end;
       end loop;
 
